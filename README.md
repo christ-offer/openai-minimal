26939b27e7ad5c1e85624ef480abaf71ebfaa2ee
Christoffer Holst <cholst@gmail.com>
2022-11-26 15:34:49 +0100

    added in edits again and models, as well as image generation

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## Usage
 
 ```ts
-const openai = new OpenAIApi('your-api-key');
+const openai = new OpenAI('your-api-key');
 
-const prompt = "/* The following is a deno library to talk to pokemon api, it is built using typescript. Start with the interrfaces needed */"
-const res = await openai.createCodeCompletion({
+const prompt = "/* Return a markdown readme file for a program that has a function called checkWeather() that uses the met.no api to get the current weather */"
+const res = await openai.createCompletion({
   model: "code-davinci-002",
   prompt: `${prompt}`,
-  max_tokens: 200,
+  max_tokens: 400,
   temperature: 0,
   top_p: 1,
   n: 1,
@@ -16,6 +16,8 @@ const res = await openai.createCodeCompletion({
   stop: "",
 });
 
+const moderation = await openai.createModeration(res.data.choices[0].text);
 
 console.log(res.choices[0].text);
+console.log(moderation.data);
 ```
--- lib/openai.ts
+++ lib/openai.ts
@@ -1,5 +1,34 @@
 // Interfaces for the request and response objects
 
+// Models
+
+interface Models {
+  data: Model[];
+  object: string; 
+}
+
+interface Model {
+  id: string;
+  object: string;
+  owned_by: string;
+  permission: string[];
+}
+
+// Completions
+
+interface Choice {
+  text: string;
+  index: number;
+  logprobs: null;
+  finish_reason: string;
+}
+
+interface Usage {
+  prompt_tokens: number;
+  completion_tokens: number;
+  total_tokens: number;
+}
+
 interface CompletionRequest {
   model: string;
   prompt: string;
@@ -20,7 +49,9 @@ interface CompletionResponse {
   choices: Choice[];
   usage: Usage;
 }
-/*
+
+// Edits
+
 interface EditRequest {
   model: string;
   input: string;
@@ -36,34 +67,78 @@ interface EditResponse {
   choices: EditChoice[];
   usage: Usage;
 }
-*/
 
 interface EditChoice {
   text: string;
   index: number;
 }
 
-interface Choice {
-  text: string;
-  index: number;
-  logprobs: null;
-  finish_reason: string;
+// Moderation
+
+interface ModerationRequest {
+  input: string;
 }
 
-interface Usage {
-  prompt_tokens: number;
-  completion_tokens: number;
-  total_tokens: number;
+interface ModerationResponse {
+  id: string;
+  model: string;
+  results: Result[];
 }
 
-export class OpenAIApi {
+interface Result {
+  categories: Categories;
+  category_scores: CategoryScores;
+  flagged: boolean;
+}
+
+interface Categories {
+  hate: boolean;
+  "hate/threatening": boolean;
+  "self-harm": boolean;
+  sexual: boolean;
+  "sexual/minors": boolean;
+  violence: boolean;
+  "violence/graphic": boolean;
+}
+
+interface CategoryScores {
+  hate: number;
+  "hate/threatening": number;
+  "self-harm": number;
+  sexual: number;
+  "sexual/minors": number;
+  violence: number;
+  "violence/graphic": number;
+}
+
+// Image generation
+
+interface ImageRequest {
+  prompt: string;
+  n: number;
+  size: string;
+  response_format: string;
+}
+
+interface ImageResponse {
+  created: number;
+  data: Data[];
+}
+
+interface Data {
+  url: string;
+}
+
+// Class for the OpenAI API
+
+export class OpenAI {
   private apiKey: string;
 
   constructor(apiKey: string) {
     this.apiKey = apiKey;
   }
 
-  async createCodeCompletion(request: CompletionRequest): Promise<CompletionResponse> {
+  async createCompletion(request: CompletionRequest): Promise<CompletionResponse> {
     const response = await fetch("https://api.openai.com/v1/completions", {
       method: "POST",
       headers: {
@@ -74,8 +149,6 @@ export class OpenAIApi {
     });
     return response.json();
   }
-  /* TODO: Not reqdy yet
-  
   async createEdit(request: EditRequest): Promise<EditResponse> {
     const response = await fetch("https://api.openai.com/v1/edits", {
       method: "POST",
@@ -87,5 +160,36 @@ export class OpenAIApi {
     });
     return response.json();
   }
-  */
+  async createImage(request: ImageRequest): Promise<ImageResponse> {
+    const response = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+      body: JSON.stringify(request),
+    });
+    return response.json();
+  }
+  async createModeration(request: ModerationRequest): Promise<ModerationResponse> {
+    const response = await fetch("https://api.openai.com/v1/moderations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+      body: JSON.stringify(request),
+    });
+    return response.json();
+  }
+  async getModels(): Promise<Models> {
+    const response = await fetch("https://api.openai.com/v1/models", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+    });
+    return response.json();
+  }
 }
