9c363a60a32d5351289ca8587f31560373e15cd4
Christoffer Holst <cholst@gmail.com>
2022-11-26 15:39:57 +0100

    seperated the types form the class export file

--- lib/openai.ts
+++ lib/openai.ts
@@ -1,135 +1,14 @@
-// Interfaces for the request and response objects
-
-// Models
-
-interface Models {
-  data: Model[];
-  object: string; 
-}
-
-interface Model {
-  id: string;
-  object: string;
-  owned_by: string;
-  permission: string[];
-}
-
-// Completions
-
-interface Choice {
-  text: string;
-  index: number;
-  logprobs: null;
-  finish_reason: string;
-}
-
-interface Usage {
-  prompt_tokens: number;
-  completion_tokens: number;
-  total_tokens: number;
-}
-
-interface CompletionRequest {
-  model: string;
-  prompt: string;
-  max_tokens: number;
-  temperature: number;
-  top_p: number;
-  n: number;
-  stream: boolean;
-  logprobs: null;
-  stop: string;
-}
-
-interface CompletionResponse {
-  id: string;
-  object: string;
-  created: number;
-  model: string;
-  choices: Choice[];
-  usage: Usage;
-}
-
-// Edits
-
-interface EditRequest {
-  model: string;
-  input: string;
-  instruction: string;
-  n: number;
-  temperature: number;
-  top_p: number;
-}  
-
-interface EditResponse {
-  object: string;
-  created: number;
-  choices: EditChoice[];
-  usage: Usage;
-}
-
-interface EditChoice {
-  text: string;
-  index: number;
-}
-
-// Moderation
-
-interface ModerationRequest {
-  input: string;
-}
-
-interface ModerationResponse {
-  id: string;
-  model: string;
-  results: Result[];
-}
-
-interface Result {
-  categories: Categories;
-  category_scores: CategoryScores;
-  flagged: boolean;
-}
-
-interface Categories {
-  hate: boolean;
-  "hate/threatening": boolean;
-  "self-harm": boolean;
-  sexual: boolean;
-  "sexual/minors": boolean;
-  violence: boolean;
-  "violence/graphic": boolean;
-}
-
-interface CategoryScores {
-  hate: number;
-  "hate/threatening": number;
-  "self-harm": number;
-  sexual: number;
-  "sexual/minors": number;
-  violence: number;
-  "violence/graphic": number;
-}
-
-// Image generation
-
-interface ImageRequest {
-  prompt: string;
-  n: number;
-  size: string;
-  response_format: string;
-}
-
-interface ImageResponse {
-  created: number;
-  data: Data[];
-}
-
-interface Data {
-  url: string;
-}
-
-// Class for the OpenAI API
+import { 
+  CompletionRequest, 
+  CompletionResponse, 
+  EditRequest, 
+  EditResponse, 
+  ImageRequest, 
+  ImageResponse, 
+  ModerationRequest, 
+  ModerationResponse, 
+  Models 
+} from "./types/types.ts";
 
 export class OpenAI {
   private apiKey: string;
@@ -137,7 +16,6 @@ export class OpenAI {
   constructor(apiKey: string) {
     this.apiKey = apiKey;
   }
-
   async createCompletion(request: CompletionRequest): Promise<CompletionResponse> {
     const response = await fetch("https://api.openai.com/v1/completions", {
       method: "POST",
---
+++ lib/types/types.ts
@@ -0,0 +1,130 @@
+// Models
+
+interface Models {
+  data: Model[];
+  object: string; 
+}
+
+interface Model {
+  id: string;
+  object: string;
+  owned_by: string;
+  permission: string[];
+}
+
+// Completions
+
+interface Choice {
+  text: string;
+  index: number;
+  logprobs: null;
+  finish_reason: string;
+}
+
+interface Usage {
+  prompt_tokens: number;
+  completion_tokens: number;
+  total_tokens: number;
+}
+
+interface CompletionRequest {
+  model: string;
+  prompt: string;
+  max_tokens: number;
+  temperature: number;
+  top_p: number;
+  n: number;
+  stream: boolean;
+  logprobs: null;
+  stop: string;
+}
+
+interface CompletionResponse {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  choices: Choice[];
+  usage: Usage;
+}
+
+// Edits
+
+interface EditRequest {
+  model: string;
+  input: string;
+  instruction: string;
+  n: number;
+  temperature: number;
+  top_p: number;
+}  
+
+interface EditResponse {
+  object: string;
+  created: number;
+  choices: EditChoice[];
+  usage: Usage;
+}
+
+interface EditChoice {
+  text: string;
+  index: number;
+}
+
+// Moderation
+
+interface ModerationRequest {
+  input: string;
+}
+
+interface ModerationResponse {
+  id: string;
+  model: string;
+  results: Result[];
+}
+
+interface Result {
+  categories: Categories;
+  category_scores: CategoryScores;
+  flagged: boolean;
+}
+
+interface Categories {
+  hate: boolean;
+  "hate/threatening": boolean;
+  "self-harm": boolean;
+  sexual: boolean;
+  "sexual/minors": boolean;
+  violence: boolean;
+  "violence/graphic": boolean;
+}
+
+interface CategoryScores {
+  hate: number;
+  "hate/threatening": number;
+  "self-harm": number;
+  sexual: number;
+  "sexual/minors": number;
+  violence: number;
+  "violence/graphic": number;
+}
+
+// Image generation
+
+interface ImageRequest {
+  prompt: string;
+  n: number;
+  size: string;
+  response_format: string;
+}
+
+interface ImageResponse {
+  created: number;
+  data: Data[];
+}
+
+interface Data {
+  url: string;
+}
+
+export type { Models, Model, CompletionRequest, CompletionResponse, EditRequest, EditResponse, ModerationRequest, ModerationResponse, ImageRequest, ImageResponse };
